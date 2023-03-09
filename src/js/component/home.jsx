3c4fecd455cf2bb6f1ed08c1e689192549b0a078
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-09 20:39:07 +0000

    fecth metodo get

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+
 
 //create your first component
 const Home = () => {
+const [tareas,setTareas] = useState()
+
+useEffect( ()=>{
+	fetch("https://assets.breatheco.de/apis/fake/todos/user/afernandez",{
+		method:"GET",
+		headers:{"Content-Type":"application-json"}
+	})
+	.then((response)=>{
+		console.log(response.ok)
+		console.log(response.status)
+		console.log(response.text)
+		return response.json()
+	})
+	.then((data) => {
+		console.log(data)
+		return setTareas(data)
+	})
+	.catch((error)=>{
+		console.log("esto es un error",error)
+	})
+},[])
+
+
 	return (
 		<div className="text-center">
 			hola 
