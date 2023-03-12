54a0a3666a65c088fc0825dda9ac06cefb690e92
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-12 18:12:34 +0000

    falta check y metodo filter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Intro from "/workspace/Todo-List-con-Fetch/src/js/component/intro.jsx";
+
 
 const URL = "https://assets.breatheco.de/apis/fake/todos/user/afernandez"
 
 
+
 const Home = () => {
-	const [inputValue, setInputValue] = useState("")
-	const [tareas, setTareas] = useState([])
+	const [inputValue, setInputValue] = useState("");
+	const [tareas, setTareas] = useState([]);
+   
 
 	const getListado = () => {
 		fetch(URL, {
@@ -13,88 +17,91 @@ const Home = () => {
 			headers: { "Content-Type": "application/json" }
 		})
 			.then((response) => {
-				console.log(response.ok)
-				console.log(response.status)
-				console.log(response.text)
+				
 				return response.json()
 			})
 			.then((data) => {
-				console.log(data)
 				setTareas(data)
 			})
 			.catch((error) => {
-				console.log("esto es un error", error)
+				console.log("error al obtener la informacion", error)
 			})
-	} 
+	}
 
-		const putTareas = (newtareas) => { 
-			fetch(URL, {
-				method: "PUT",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(newtareas), // lo tiene que recibir en formato stringify este body con  formato de texto por la forma de comunicacion https
+	const putTareas = (newtareas) => {
+		fetch(URL, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(newtareas), // lo tiene que recibir en formato stringify este body con  formato de texto por la forma de comunicacion https
+		})
+			.then(() => {
+				console.log("tarea enviada");
+				getListado()
 			})
-				.then(() => {
-					console.log("tarea enviada");
-					getListado()
-				})
-	
-				.catch((error) => {
-					console.log("error de envio ", error);
-				});
-		}
 
-	useEffect(() => {getListado()}, []) 
+			.catch((error) => {
+				console.log("error de envio ", error);
+			});
+	}
+
+
+	useEffect( getListado , [])
 
 	const añadirTarea = () => {
-       const datos = {label: inputValue , done : false};
-	   const nuevaTarea = [...tareas,datos];
-	   putTareas(nuevaTarea);
-	
+		const datos = { label: inputValue, done: false };
+		const nuevaTarea = [...tareas, datos];
+		putTareas(nuevaTarea);
+
 	}
 
-	const borrarTarea = (indice) =>{
+	const borrarTarea = (index) => {
 		const datos = [...tareas] // 1º creo una copia de las tareas que ya tengo (array)/estado
-		datos.splice(indice,1); //2º metodo splice empiezo en mi indice(donde me encuentro) y borro solo 1
-		setTareas(datos); // actualizar el estado de las tareas con los nuevos datos
-		 putTareas(datos); // meto los datos en el servidor
-		 getListado(); // me los traigo del server 
-         console.log("datos borrados no más",datos[indice])
-	}
-	const check =(indice) => {
-	const datos = [...tareas]; //1º el indice para recorrer el array 
-	; //2º accedo a la key done  y luego lo negamos para ir cambiando true/false
-	
-	putTareas(datos);
-	getListado();
-                     }
-	return ( // poner debajo de lalinea 58 el loading 
-		<div className="text-center"> 
-			<h1> To do list with Fetch</h1> 
-		
-			<input type="text" value={inputValue}
-				onChange={(element) => setInputValue(element.target.value)}
-				onKeyDown={(e) => { // en React este paramentro representa el evento de tecla y key es el tipo de tecla 
-					if (e.key === "Enter" && inputValue.length >= 2) {
-						añadirTarea()
-						setInputValue(" ")
-					}}
-				     }
-				placeholder="escriba la tarea" />
+		datos.splice(index,1); //2º metodo filter, devuelve un nuevo array pero eliminando el indice que le indicamos 
+		setTareas(datos); // ddespues actualizo el estado de las tareas con los nuevos datos
+		putTareas(datos); // meto los datos en el servidor
+		getListado();
 
+	}
+	const check = (indice) => {
+		const datos = { done: true}; //1º el indice para recorrer el array 
+		const tareaLista =[...tareas, datos]; //2º accedo a la key done  y luego lo negamos para ir cambiando true/false
+		putTareas(tareaLista[indice]);
+		getListado();
+	}
+
+
+	return ( // poner debajo de lalinea 58 el loading 
+		<div className="container">
+			< Intro />
+
+			<div className="row">
+				<input type="text" value={inputValue}
+					onChange={(element) => setInputValue(element.target.value)}
+					onKeyDown={(e) => { // en React este paramentro representa el evento de tecla y key es el tipo de tecla 
+						if (e.key === "Enter" && inputValue.length >= 2) {
+							añadirTarea()
+							setInputValue(" ")
+						}
+					}
+					}
+					placeholder="Escribe tus tareas pendientes " />
+			</div>
 
 			<ul className="list-group">
-				{tareas.map((tarea,indice) => (
-					<li className="list-group-item">
+				{tareas.map((tarea) => (
+					<li key={tarea.label} className="list-group-item">
 						{tarea.label}
-						<i class="fa-solid fa-trash" onClick={borrarTarea(indice)}></i>
-						<i class="fa-solid fa-check-to-slot" onClick={check(indice)}></i>
+						<i className="fa-solid fa-trash" onClick={borrarTarea}></i>
+						<i id="iconoCheck" class="fa-solid fa-check-to-slot" onClick={check}></i>
 
 					</li>
 				))}
 			</ul>
-
+                      
 		</div>
 	);
 };
 
 export default Home;
+
+
---
+++ src/js/component/intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Intro =  () => {
+
+    return (
+
+        <div className="row" >
+        <h1 className="text-center mt-5" id="intro"> Lista de Tareas ft Fetch</h1> 
+        <p className="text-center my-5" >  Por favor, introduce tareas que tengas pendientes por realizar y pulsa Enter!. GGWP</p>
+        </div>
+    )
+
+}
+
+
+
+
+
+
+export default Intro;
